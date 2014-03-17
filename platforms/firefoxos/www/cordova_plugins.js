74447379ee795c892ade04c9d093422b1e90013d
Guilherme Costa Berghauser <tortoyoyo@gmail.com>
2014-03-16 21:02:21 -0300

    InicialCommit
    Cordova Projetct - Platforms: Android and FirefoxOs

---
+++ platforms/firefoxos/www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/src/firefoxos/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer-impl",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.device-motion": "0.2.6"
+}
+// BOTTOM OF METADATA
+});
